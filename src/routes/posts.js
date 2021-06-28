76215b6a5cbadf5baa3382c14fc176509afae12b
Teodor Solca <t-c.solca@metro.digital>
2021-06-28 10:33:42 +0300

    changed path

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,14 @@
 const express = require('express'),
   router = express.Router(),
-  Utils = require( './../utils/utils' );
+  Utils = require( './../utils/utils' ),
+  path = require("path");
+
+const directoryName = "./src/posts"
+const postPath = (process.env.LAMBDA_TASK_ROOT)? path.resolve(process.env.LAMBDA_TASK_ROOT, directoryName):path.resolve(__dirname, directoryName)
 
 
 router.get('/getAllPosts',(req,res) => {
-    Utils.readAllFilesFromDirectory('src/posts')
+    Utils.readAllFilesFromDirectory(postPath)
     .then((filenames) => {
       let enhancedFilesNames = Utils.enhanceFilesNames(filenames)
       res.status(200).send(enhancedFilesNames)
@@ -15,13 +19,13 @@ router.get('/getAllPosts',(req,res) => {
 router.get('/getPosts/:fileNameURL',(req,res) => {
     let fileNameURL = req.params.fileNameURL;
 
-    Utils.readAllFilesFromDirectory('src/posts')
+    Utils.readAllFilesFromDirectory(postPath)
     .then((fileNames) => {
       let enhancedFilesNames = Utils.enhanceFilesNames(fileNames),
         fileName = Utils.getFileName(fileNameURL, enhancedFilesNames)
 
         if (fileName) {
-          Utils.readFile(`src/posts/${fileName}`)
+          Utils.readFile(`${postPath}/${fileName}`)
           .then((data) => {
             res.status(200).send(data)
           })
